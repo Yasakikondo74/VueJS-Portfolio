477a38897d16f105114ea2b95c962c34bb95baae
Yasaki74 <83692124+Yasakikondo74@users.noreply.github.com>
2025-04-22 01:11:14 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VueJS-Portfolio/',
+  base: '/VueJS-Portfolio',
   plugins: [
     vue(),
     //VueDevTools(),
