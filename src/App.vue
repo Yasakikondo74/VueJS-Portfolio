fa69057067b523bd8e46c401f43f680c0549c929
Yasaki74 <83692124+Yasakikondo74@users.noreply.github.com>
2025-04-24 03:30:30 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,5 @@
 <script setup>
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
 import { RouterView } from "vue-router"
 </script>
